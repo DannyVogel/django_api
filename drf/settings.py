0702c3ef7d88aaefa4428bad47f6d5ddf8497bac
Danny <danny.hj.vogel@gmail.com>
2025-01-15 23:21:33 +0100

    Remove Python cache files

--- drf/settings.py
+++ drf/settings.py
@@ -131,5 +131,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
 }
